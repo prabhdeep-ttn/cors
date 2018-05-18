ceab56e52734b03492604cec251852c6d3b8255b
prabhdeep.puri <prabhdeep.puri@net-effect.com>
2018-05-19 02:02:02 +0530

    Added CORS Excersize Helper Code

--- ui.apps/src/main/content/jcr_root/apps/cors/components/content/html/clientlib/js/cors-request.js
+++ ui.apps/src/main/content/jcr_root/apps/cors/components/content/html/clientlib/js/cors-request.js
@@ -1,19 +1,4 @@
 var requestUrl = function(componentId, url){
-    "use strict";
-    $.ajax({
-        url: "http://localhost:6502/bin/testcors", 
-        beforeSend: function(xhr, settings){
-            console.log("Requesting URL: ",settings.url);
-            xhr.setRequestHeader ("Authorization", "Basic YWRtaW46YWRtaW4=");
-        },
-        success: function(result){
-            console.log(result);
-            $('#'+componentId).append(result);
-        },
-        error: function(xhr,status,error){
-            console.log("Error: ", xhr,status,error)
-            $('#'+componentId).append("Error Occurred!! "+error);
-        }
-    });
+    //TODO: Send a request with login info to fetch response
 };
 
